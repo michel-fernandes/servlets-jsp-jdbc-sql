43234c6d0261fc3fc5894fcd0b4abd7123482d25
Michel Fernandes <michelfernandes3@gmail.com>
2022-10-15 20:58:18 -0300

    Atualizado email do git, aff

--- Projeto-exemplo-JSP/src/main/java/servlets/ServletUsuarioController.java
+++ Projeto-exemplo-JSP/src/main/java/servlets/ServletUsuarioController.java
@@ -58,7 +58,7 @@ public class ServletUsuarioController  extends ServletGenericUtil{
 			}else if(acao.equals("downloadImagem")){
 				Long id = Long.parseLong(request.getParameter("id"));
 				ModelLogin user = usuarioDAO.consultarId(id, super.getUserLogado(request));		
-				
+				//cadastro-usuario download da imagem do perfil
 				if(user.getImagem()!="" && !user.getImagem().isEmpty()) {
 					response.setHeader("Content-Disposition", "attachment;filename=download_image."+ user.getFormatoImagem());
 					response.getOutputStream().write(new Base64().decodeBase64(user.getImagem().split("\\,")[1]));
